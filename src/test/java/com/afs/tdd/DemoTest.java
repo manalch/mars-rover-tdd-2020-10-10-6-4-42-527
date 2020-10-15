1ce27786601e4d73a009adcaa1f1c585ed2284a7
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 20:39:59 +0800

    [TDD-MarsRover] [Joseph] test: applied test when command is R given 0, 0, E

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -92,4 +92,15 @@ class DemoTest {
         assertEquals(0, marsRover.getLocationY());
         assertEquals("N", marsRover.getDirection());
     }
+
+    @Test
+    void should_return_0_locationX_0_locationY_S_heading_when_command_given_is_R() {
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+
+        marsRover.executeCommand("R");
+
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("S", marsRover.getDirection());
+    }
 }
