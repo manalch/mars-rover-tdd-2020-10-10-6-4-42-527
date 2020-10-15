74e30ddc0b3fa2d65d1f0e70918491f7575ea6c0
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-15 19:25:37 +0800

    [TDD-MarsRover] [Joseph] test: applied test when_command_given_is_M

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,17 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_return_0_locationX_1_locationY_N_heading_when_command_given_is_M() {
+        MarsRover marsRover = new MarsRover(0, 0, "N");
 
+        marsRover.executeCommand("M");
+
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(1, marsRover.getLocationY());
+        assertEquals("N", marsRover.getDirection());
     }
 }
