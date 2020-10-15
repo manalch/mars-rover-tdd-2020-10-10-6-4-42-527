a8ab23e7ccda02264261499148d88b31afa6436f
CHRISTIAN JOSEPH MANALAC (SHP-IRIS-ISD-OOCLL/MNL) <MANALCH@OOCL.COM>
2020-10-16 00:20:58 +0800

    [TDD-MarsRover] [Joseph] refactor: refactored MarsRover class and included constants

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,11 +1,36 @@
 package com.afs.tdd;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MarsRover {
     private int locationX;
     private int locationY;
     private String direction;
+    private static final String NORTH = "N";
+    private static final String SOUTH = "S";
+    private static final String EAST = "E";
+    private static final String WEST = "W";
+    private static final String TURN_LEFT_COMMAND = "L";
+    private static final String TURN_RIGHT_COMMAND = "R";
+    private static final String EMPTY_STRING = "";
+    private static final String MOVE_COMMAND = "M";
+    private static final Map<String, String> LEFT_TURN_MAP = new HashMap<>();
+    private static final HashMap<String, String> RIGHT_TURN_MAP = new HashMap<>();
+    private static final List<String> COMMAND_LIST = Arrays.asList(MOVE_COMMAND, TURN_LEFT_COMMAND, TURN_RIGHT_COMMAND);
+
+    static {
+        LEFT_TURN_MAP.put(NORTH, WEST);
+        LEFT_TURN_MAP.put(SOUTH, EAST);
+        LEFT_TURN_MAP.put(EAST, NORTH);
+        LEFT_TURN_MAP.put(WEST, SOUTH);
+        RIGHT_TURN_MAP.put(NORTH, EAST);
+        RIGHT_TURN_MAP.put(SOUTH, WEST);
+        RIGHT_TURN_MAP.put(EAST, SOUTH);
+        RIGHT_TURN_MAP.put(WEST, NORTH);
+    }
 
     public MarsRover(int locationX, int locationY, String direction) {
         this.locationX = locationX;
@@ -14,7 +39,7 @@ public class MarsRover {
     }
 
     public void executeCommand(String commands) {
-        Arrays.stream(commands.split(""))
+        Arrays.stream(commands.split(EMPTY_STRING))
                 .forEach(command -> {
                     try {
                         checkValidCommand(command);
@@ -27,60 +52,37 @@ public class MarsRover {
     }
 
     public void checkValidCommand(String command) throws CommandNotDefinedException {
-        if (!Arrays.asList("M", "L", "R").contains(command)) {
+        if (!COMMAND_LIST.contains(command)) {
             throw new CommandNotDefinedException("Command " + command + " Not Defined");
         }
     }
 
     private void move(String command) {
-        if (command.equals("M")) {
-            if (direction.equals("N")) {
+        if (!command.equals(MOVE_COMMAND)) {
+            return;
+        }
+        switch (direction) {
+            case NORTH:
                 locationY++;
-            }
-            if (direction.equals("S")) {
+                break;
+            case SOUTH:
                 locationY--;
-            }
-            if (direction.equals("E")) {
+                break;
+            case EAST:
                 locationX++;
-            }
-            if (direction.equals("W")) {
+                break;
+            case WEST:
                 locationX--;
-            }
+                break;
         }
     }
 
     private void turn(String command) {
-        if (command.equals("L")) {
-            if (direction.equals("N")) {
-                direction = "W";
-                return;
-            }
-            if (direction.equals("S")) {
-                direction = "E";
-                return;
-            }
-            if (direction.equals("E")) {
-                direction = "N";
-            }
-            if (direction.equals("W")) {
-                direction = "S";
-            }
+        if (command.equals(TURN_LEFT_COMMAND)) {
+            direction = LEFT_TURN_MAP.get(direction);
         }
-        if (command.equals("R")) {
-            if (direction.equals("N")) {
-                direction = "E";
-                return;
-            }
-            if (direction.equals("S")) {
-                direction = "W";
-                return;
-            }
-            if (direction.equals("E")) {
-                direction = "S";
-            }
-            if (direction.equals("W")) {
-                direction = "N";
-            }
+        if (command.equals(TURN_RIGHT_COMMAND)) {
+            direction = RIGHT_TURN_MAP.get(direction);
         }
     }
 
